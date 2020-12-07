41ba7ad8864740ae1707c182cf755127e2fc9a74
j701u05 <j701u05@J701AT05.J504.kansai-center.ac.jp>
2020-12-07 16:16:41 +0900

    chap2

--- src/chap2/CompareTreeSetSample.java
+++ src/chap2/CompareTreeSetSample.java
@@ -1,7 +1,15 @@
 package chap2;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CompareTreeSetSample {
 	public static void main(String[] args) {
+		Set<Product> set = new TreeSet<Product>();
 
+		set.add(new Product(3, "B"));
+		set.add(new Product(1, "C"));
+		set.add(new Product(2, "A"));
+		System.out.println(set);
 	}
 }
