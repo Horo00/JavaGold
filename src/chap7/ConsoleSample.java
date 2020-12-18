14598b5b91edcedf50b5bc2482209ef2e1855a5a
j701u05 <j701u05@J701AT05.J504.kansai-center.ac.jp>
2020-12-18 16:06:38 +0900

    sample

--- src/chap7/ConsoleSample.java
+++ src/chap7/ConsoleSample.java
@@ -2,16 +2,20 @@ package chap7;
 
 import java.io.Console;
 import java.io.Serializable;
+import java.nio.file.FileSystems;
 
 public class ConsoleSample implements Serializable{
 	public static void main(String[] args) {
 		Console console = System.console();
 		if (console != null) {
 			String line = console.readLine();
-			return;
+			return ;
 		}
 		System.out.println(console);
+		
+		System.out.println(System.out);
 
-		console.readPassword("Password:");
+//		console.readPassword("Password:");
+		System.out.println(FileSystems.getDefault());
 	}
 }
